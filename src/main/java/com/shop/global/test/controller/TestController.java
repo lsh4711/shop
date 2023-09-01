3796df5bb903f8630c10462b100829796a10bd58
lsh4711 <dowkwll@naver.com>
2023-09-02 00:22:26 +0900

    Add devtools and app yml

---
+++ src/main/java/com/shop/global/test/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.shop.global.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity test() {
+        return ResponseEntity.ok("테스트");
+    }
+}
