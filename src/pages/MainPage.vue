3ba659a28f5b26588a8719e2ae18173f1c789c38
lsh4711 <dowkwll@naver.com>
2023-09-16 06:37:48 +0900

    feat: 메인 페이지 버튼 드래그 방지

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="img" class="cover" :style="{ backgroundImage: `url(${image.src})` }">
     <div class="position-absolute bottom-0 start-50 translate-middle">
-      <RouterLink to="/marts">
+      <RouterLink to="/marts" class="no-drag" :ondragstart="noDrag">
         <button class="custom-btn btn-1"><span>쇼핑하러 가기</span></button>
       </RouterLink>
     </div>
@@ -12,6 +12,10 @@
 import { loadImage } from "@/utils/Loading/script/loading";
 
 const image = loadImage("/images/cover.jpg");
+
+const noDrag = (event: MouseEvent) => {
+  event.preventDefault();
+};
 </script>
 
 <style scoped>
@@ -22,5 +26,12 @@ const image = loadImage("/images/cover.jpg");
   background-size: cover;
   width: 100%;
   height: 100%;
+}
+
+.no-drag {
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
 }
 </style>
